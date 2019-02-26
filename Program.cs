1d1f5f6120557b1237bea00cda113f5e7a6e8ed9
jojokabin <egorushkanutslove@gmail.com>
2019-02-26 12:29:00 +0700

    mane

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите А: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите действие:");
+
+            ConsoleKeyInfo Key = Console.ReadKey();
+
+
+          
+            char selection = Key.KeyChar;
+            switch (selection)
+            {
+ 
+   
+            }
+            Console.ReadKey();
+        }
+    }
+}
