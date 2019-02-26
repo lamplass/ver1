dc4ff1ab655504eed5abdaf78d050990c1f760d9
jojokabin <egorushkanutslove@gmail.com>
2019-02-26 12:35:49 +0700

    minus

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace laba2_1
             Console.Write("Введите B: ");
             int b = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите действие:");
+            System.Console.WriteLine();
 
             ConsoleKeyInfo Key = Console.ReadKey();
 
@@ -23,12 +24,12 @@ namespace laba2_1
             char selection = Key.KeyChar;
             switch (selection)
             {
-                case '+':
+                case '-':
 
                     int x = a;
                     int y = b;
 
-                    int z = x + y;
+                    int z = x - y;
                     Console.WriteLine(z);
                     break;
 
